87e6137e4a9119d652f14a2a66267e7f74d40e2a
s2umasha <113391475+s2umasha@users.noreply.github.com>
2024-02-13 18:56:33 -0500

    Update cypress.config.js

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -1,10 +1,25 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // add custom commands and event listeners here
     },
-    "baseUrl": "http://localhost:3000"
+    baseUrl: 'http://localhost:3000', // Adjust this according to your local/dev environment
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/index.js',
+    env: {
+      // Define environment variables for your tests here
+      // For example, API endpoints, user credentials for different roles, etc.
+    },
+  },
+  // Consider adding configurations for video and screenshot options if needed
+  video: true,
+  screenshotsFolder: 'cypress/screenshots',
+  videosFolder: 'cypress/videos',
+  // Configure retries for flaky tests
+  retries: {
+    runMode: 2,
+    openMode: 0,
   },
 });
