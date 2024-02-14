47784957e51f84462ac53984e92cda2cddbffd03
chris0517 <chris92563@outlook.com>
2024-02-13 19:16:40 -0500

    cypress and jest config files

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -1,25 +1,10 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // add custom commands and event listeners here
+      // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000', // Adjust this according to your local/dev environment
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/index.js',
-    env: {
-      // Define environment variables for your tests here
-      // For example, API endpoints, user credentials for different roles, etc.
-    },
-  },
-  // Consider adding configurations for video and screenshot options if needed
-  video: true,
-  screenshotsFolder: 'cypress/screenshots',
-  videosFolder: 'cypress/videos',
-  // Configure retries for flaky tests
-  retries: {
-    runMode: 2,
-    openMode: 0,
-  },
-});
+    "baseUrl": "http://localhost:3000"
+  } 
+}); 
--- client/jest.config.js
+++ client/jest.config.js
@@ -1,29 +1,6 @@
 module.exports = {
-  clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'], // Setup file for enzyme/react-testing-library configurations
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // For CSS Modules
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js', // Mock static file imports
-  },
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    // If you're using TypeScript, make sure ts-jest is configured properly
-  },
-  // Optionally include coverage thresholds
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: -10,
+    transform: {
+      "^.+\\.(js|jsx)$": "babel-jest",
     },
-  },
-  // Configure test paths and patterns
-  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
-  // Ignore patterns
-  testPathIgnorePatterns: ['/node_modules/', '/public/'],
-};
+    testEnvironment: 'jsdom',
+  };
