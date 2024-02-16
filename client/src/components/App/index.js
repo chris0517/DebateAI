9bc8f7b852ea0bbe34309e0c7607b141226107f4
chris0517 <chris92563@outlook.com>
2024-02-15 23:56:04 -0500

    Landing page

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import Review from './Review';
+import * as React from 'react';
+import {Box, Grid } from '@mui/material';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from '../Landing';
 
-function App() {
-  return <Review />;
+
+const App = () => {
+
+
+  return (
+      <div>
+        <Grid>
+          <Box 
+            sx={{  width: '100%',
+            height: 10500, backgroundColor: '#000'}}>
+            <Router>
+              <div>
+                <Routes>
+                <Route path="/" element={<Landing />} />
+                {/* <Route path="/Search" element={<Search />} /> */}
+                </Routes>
+              </div>
+            </Router>
+
+
+
+          </Box>
+        </Grid>
+        
+      </div>
+
+    
+
+  );
 }
-
 export default App;
